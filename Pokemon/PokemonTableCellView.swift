6fa7a0ac3fa1196d86cfcc24ce00e3c8e2d07ed2
蘇健豪 <jenhausu@icloud.com>
2022-08-31 01:21:38 +0800

    fix: all view can trigger onTap

--- Pokemon/PokemonTableCellView.swift
+++ Pokemon/PokemonTableCellView.swift
@@ -77,6 +77,7 @@ struct PokemonTableCellView: View {
                 } catch { }
             }
         }
+        .contentShape(Rectangle())
         .onTapGesture {
             routeManager.selectedPokemon = data
             routeManager.isDetailViewPresented = true
